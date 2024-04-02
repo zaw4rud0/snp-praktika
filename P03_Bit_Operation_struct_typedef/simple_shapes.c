672c054ad089df6184cabebf8e295408d6857697
Didnt Read Readme <didnt.read.readme@example.com>
2024-04-02 11:31:49 +0200

    Changes

--- P03_Bit_Operation_struct_typedef/simple_shapes.c
+++ P03_Bit_Operation_struct_typedef/simple_shapes.c
@@ -27,21 +27,23 @@ typedef struct {
 
 void paint(Graphic graphic) {
     double radius = graphic.size / 2.0;
-    int i,j;
+    int i, j;
 
-    for (i = 0; i <= 2 * radius; i++){
-        for (j = 0; j <= 2 * radius; j++){
-            switch(graphic.shape) {
-                case RECTANGLE: printf("%s*" RESET, graphic.color); break;
+    for (i = 0; i <= 2 * radius; i++) {
+        for (j = 0; j <= 2 * radius; j++) {
+            switch (graphic.shape) {
+                case RECTANGLE:
+                    printf("%s*" RESET, graphic.color);
+                    break;
                 case OVAL: {
                     double distance = sqrt((i - radius) * (i - radius) + (j - radius) * (j - radius));
                     if (distance > radius - 0.5 && distance < radius + 0.5) {
                         printf("%s*" RESET, graphic.color);
-                    } else { 
+                    } else {
                         printf(" ");
                     }
-                } break;
-
+                    break;
+                }
             }
         }
         printf("\n");
@@ -50,29 +52,42 @@ void paint(Graphic graphic) {
 
 int main() {
     Graphic graphic;
-    int inputShape, inputColor;
+    int inputShape, inputColor, inputSize;
     char *colors[] = {RED, GRN, YEL};
 
     do {
         printf("Geben Sie die gewünschte Form an [OVAL=0 | RECTANGLE=1]:");
         scanf("%d", &inputShape);
         // Students: store the input in graphic
-        graphic.shape = (Shape)inputShape;
+        if (inputShape > 1 || inputShape < 0) {
+            printf("Ungültige Form!");
+            continue;
+        }
+        graphic.shape = (Shape) inputShape;
 
         printf("Geben Sie die gewünschte Grösse an:");
-        scanf("%u", &graphic.size);
+        scanf("%u", &inputSize);
         // Students: store the input in graphic
+        if (inputSize < 0) {
+            printf("Ungültige Grösse!");
+            continue;
+        }
+        graphic.size = inputSize - 1;
 
-        printf("Geben Sie die gewünschte Farb an [RED=0 | GREEN=1 | YELLOW=2]:");
+        printf("Geben Sie die gewünschte Farbe an [RED=0 | GREEN=1 | YELLOW=2]:");
         scanf("%d", &inputColor);
         // Students: store the input in graphic
+        if (inputColor > 2 || inputColor < 0) {
+            printf("Ungültige Farbe!");
+            continue;
+        }
         graphic.color = colors[inputColor];
-        
+
         paint(graphic);
-        
-        while(getchar() != '\n'); // empty buffer
-        printf("\nMöchten sie weiter machen oder abbrechen? [(n)ext|(q)uit] ");
-    } while(getchar() == 'n');
+
+        while (getchar() != '\n'); // empty buffer
+        printf("\nMöchten Sie weiter machen oder abbrechen? [(n)ext|(q)uit] ");
+    } while (getchar() != 'q');
 
     printf("Byebye..\n");
     return EXIT_SUCCESS;
